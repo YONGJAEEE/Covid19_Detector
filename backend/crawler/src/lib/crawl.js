d3e2e73f2e16f66de7b67224a3914f5a4eb2725e
TitanV <a01098930999@gmail.com>
2020-11-24 03:50:36 +0900

    add - 크롤링 코드 완성

---
+++ backend/crawler/src/lib/crawl.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+exports.crawlGetDatas = (async (link,trNum,trData,trDate) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(link);     
+
+  // tr태그의 개수
+  const number = await page.$$eval(trNum, (data) => data.length);
+  let data = [],date = [];
+
+  for (let i = 1; i <= number; i++) {
+    data.push( await page.$eval(`${trNum}:nth-child(${i}${trData}`, element => { return element.textContent; }) )
+    date.push( await page.$eval(`${trNum}:nth-child(${i}${trDate}`, element => { return element.textContent; }) )
+  }
+
+
+  console.log(data);
+  console.log(date);
+
+
+  await browser.close();
+});
