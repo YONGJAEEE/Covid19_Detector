3eedfbdea4c43729493ee82a53a10e290b2e9675
Jominseo <minseocho0309@gmail.com>
2020-11-24 14:19:37 +0900

    [add] drawer

---
+++ Android/Covid19Detector/app/src/main/java/com/example/covid19detector/view/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid19detector.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covid19detector.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
