f98a98cc8c1c70a0fbb8dea335f099245ebde596
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-11-24 16:07:55 +0900

    검색 기능 구현

---
+++ Android/Covid19Detector/app/src/main/java/com/example/covid19detector/adapter/JusoAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.covid19detector.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.covid19detector.R
+import com.example.covid19detector.model.Documents
+
+class JusoAdapter(val AdressList : ArrayList<Documents>) : RecyclerView.Adapter<JusoAdapter.Holder>(){
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val jusoText = itemView.findViewById<TextView>(R.id.tv_juso)
+
+        fun bind(juso : Documents) {
+            jusoText.text = juso.address_name
+            itemView.setOnClickListener{
+
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.adressitem, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(AdressList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return AdressList.size
+    }
+}
